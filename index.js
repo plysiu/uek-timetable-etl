7fc11aef8959ec3ebf254f450d5d8885e2b00f42
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-05-08 10:12:51 +0200

    Added setInterval

--- index.js
+++ index.js
@@ -4,80 +4,151 @@ var models = require('uekplan-models'),
     util = require('util');
 
 
-models.sequelize.sync()
-    .then(()=> {
-        console.log('Establishing connection to database');
-        if (fs.existsSync('./data.json')) {
-            console.log('Reading data from cache');
-            return Promise.resolve(JSON.parse(fs.readFileSync('./data.json')));
-        } else {
-            return require('./extract').downloadAll();
-        }
-    })
-    .then((data)=> {
-        console.log('Saving data to cache');
-        fs.writeFileSync('./data.json', JSON.stringify(data));
-        return Promise.resolve(data);
-    })
-    .then((data)=> {
-        return require('./transformLabels')(data);
-    })
-    .then((data)=> {
-        return new Promise((resolve, reject)=> {
-            return require('./labels')
-                .updateLabels(data.labels)
-                .then((labels)=> {
+var neverEndingStory = ()=> {
 
-                    delete data.list;
-                    delete data.sections;
-                    data.labels = labels;
-                    resolve(data);
-                }).catch((err)=> {
-                    reject(err)
+    return new Promise((resolve, reject)=> {
+        require('./extract').downloadAll()
+            .then((data)=> {
+                return require('./transformLabels')(data);
+            })
+            .then((data)=> {
+                return new Promise((resolve, reject)=> {
+                    return require('./labels')
+                        .updateLabels(data.labels)
+                        .then((labels)=> {
+
+                            delete data.list;
+                            delete data.sections;
+                            data.labels = labels;
+                            resolve(data);
+                        }).catch((err)=> {
+                            reject(err)
+                        });
                 });
-        });
-    })
-    .then((data)=> {
-        return require('./transformEvents')(data);
-    })
-    .then((data)=> {
-        require('./exceptions')(data.exceptions)
-            .then(()=> {
-                return require('./temporary')(data.events)
-            })
-            .then(()=> {
-                return require('./diff').moveFromTempToEvents();
             })
             .then((data)=> {
-                console.log(data);
-                return require('./diff').setUnDeletedInEventsWhenExists();
+                return require('./transformEvents')(data);
             })
             .then((data)=> {
-                console.log(data);
+                require('./exceptions')(data.exceptions)
+                    .then(()=> {
+                        return require('./temporary')(data.events)
+                    })
+                    .then(()=> {
+                        return require('./diff').moveFromTempToEvents();
+                    })
+                    .then((data)=> {
+                        console.log(data);
+                        return require('./diff').setUnDeletedInEventsWhenExists();
+                    })
+                    .then((data)=> {
+                        console.log(data);
 
-                return require('./diff').setDeleteInEventsWhenNotExists();
-            })
-
-            .then((data)=> {
-                console.log(data);
-                process.exit();
-            })
-            .catch((e)=> {
-                console.log(e.message);              // "Hello"
-                console.log(e.name);                 // "TypeError"
-                console.log(e.fileName);             // "someFile.js"
-                console.log(e.lineNumber);           // 10
-                console.log(e.columnNumber);         // 0
-                console.log(e.stack);
-                process.exit();
+                        require('./diff').setDeleteInEventsWhenNotExists();
+                        resolve();
+                    })
+                    .then((data)=> {
+                        console.log(data);
+                    })
+                    .catch((e)=> {
+                        console.log(e.message);              // "Hello"
+                        console.log(e.name);                 // "TypeError"
+                        console.log(e.fileName);             // "someFile.js"
+                        console.log(e.lineNumber);           // 10
+                        console.log(e.columnNumber);         // 0
+                        console.log(e.stack);
+                        reject(err);
+                    });
             });
-    })
-    .catch((e)=> {
-        console.log(e.message);              // "Hello"
-        console.log(e.name);                 // "TypeError"
-        console.log(e.fileName);             // "someFile.js"
-        console.log(e.lineNumber);           // 10
-        console.log(e.columnNumber);         // 0
-        console.log(e.stack);
-        process.exit();
     });
+};
+
+models.sequelize.sync().then(()=> {
+
+    neverEndingStory();
+
+    setInterval(()=> {
+        neverEndingStory();
+
+    }, 1000 * 60 * 60 * 6);
+
+
+});
+
+
+// models.sequelize.sync().then(()=> {
+//     console.log('Establishing connection to database');
+//     if (fs.existsSync('./data.json')) {
+//         console.log('Reading data from cache');
+//         return Promise.resolve(JSON.parse(fs.readFileSync('./data.json')));
+//     } else {
+//         return require('./extract').downloadAll();
+//     }
+// })
+//     .then((data)=> {
+//         console.log('Saving data to cache');
+//         fs.writeFileSync('./data.json', JSON.stringify(data));
+//         return Promise.resolve(data);
+//     })
+//     .then((data)=> {
+//         return require('./transformLabels')(data);
+//     })
+//     .then((data)=> {
+//         return new Promise((resolve, reject)=> {
+//             return require('./labels')
+//                 .updateLabels(data.labels)
+//                 .then((labels)=> {
+//
+//                     delete data.list;
+//                     delete data.sections;
+//                     data.labels = labels;
+//                     resolve(data);
+//                 }).catch((err)=> {
+//                     reject(err)
+//                 });
+//         });
+//     })
+//     .then((data)=> {
+//         return require('./transformEvents')(data);
+//     })
+//     .then((data)=> {
+//         require('./exceptions')(data.exceptions)
+//             .then(()=> {
+//                 return require('./temporary')(data.events)
+//             })
+//             .then(()=> {
+//                 return require('./diff').moveFromTempToEvents();
+//             })
+//             .then((data)=> {
+//                 console.log(data);
+//                 return require('./diff').setUnDeletedInEventsWhenExists();
+//             })
+//             .then((data)=> {
+//                 console.log(data);
+//
+//                 return require('./diff').setDeleteInEventsWhenNotExists();
+//             })
+//
+//             .then((data)=> {
+//                 console.log(data);
+//                 process.exit();
+//             })
+//             .catch((e)=> {
+//                 console.log(e.message);              // "Hello"
+//                 console.log(e.name);                 // "TypeError"
+//                 console.log(e.fileName);             // "someFile.js"
+//                 console.log(e.lineNumber);           // 10
+//                 console.log(e.columnNumber);         // 0
+//                 console.log(e.stack);
+//                 process.exit();
+//             });
+//     })
+//     .catch((e)=> {
+//         console.log(e.message);              // "Hello"
+//         console.log(e.name);                 // "TypeError"
+//         console.log(e.fileName);             // "someFile.js"
+//         console.log(e.lineNumber);           // 10
+//         console.log(e.columnNumber);         // 0
+//         console.log(e.stack);
+//         process.exit();
+//     });
